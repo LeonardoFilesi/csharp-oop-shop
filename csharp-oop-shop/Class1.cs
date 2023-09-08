fbc1a903d8aa7b228d37054936a5a41f276aa6ed
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-09-08 16:57:38 +0200

    Aggiungere i file di progetto.

---
+++ csharp-oop-shop/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    public class Prodotto
+    {
+        // ATTRIBUTI:
+        private int Codice;
+        private string Nome { get; set; }
+        private string Descrizione { get; set; }
+        private decimal Prezzo { get; set; }
+        private decimal Iva { get; set; }
+
+        // COSTRUTTORE
+        public Prodotto( string nome, string descrizione, decimal prezzo, decimal iva)
+        {
+            this.Codice = new Random().Next(0001, 9999);
+            this.Nome = nome;
+            this.Descrizione = descrizione;
+            this.Prezzo = prezzo;
+            this.Iva = iva;
+        }
+
+        // GETTER
+
+        public int codice
+        {
+            get { return Codice; }
+        }
+        public string nome
+        {
+            get { return Nome; }
+        }
+        public string descrizione
+        {
+            get { return Descrizione; }
+        }
+        public decimal prezzo
+        { 
+            get { return Prezzo; } 
+        }
+        public decimal prezzoConIva
+        {
+            get { return Prezzo + (Prezzo / 100 * Iva); }
+        }
+        public decimal iva
+        {
+            get { return (prezzo / 100 * Iva);
+        }
+    }
+}
---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,12 @@
+﻿using csharp_oop_shop;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        {
+            Prodotto actionFigure = new Prodotto("action figure", "Stormtrooper", 143, 22);
+
+            Console.WriteLine($"La nuovissima {actionFigure.nome} rappresenta uno {actionFigure.descritione} nella sua tipica posa. Il prezzo è {actionFigure.prezzoConIva}$ di cui iva {actionFigure.iva}");
+        }
+    }
+}
